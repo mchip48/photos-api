feef388544d54512d8c1d8a22c6db4468c0fc4d5
Matthew Chipkin <mchipkin21@gmail.com>
2025-11-17 13:05:59 -0600

    Implemented index, show, create actions in photos controller without corresponding routes

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,32 @@
 class PhotosController < ApplicationController
+
+  def index
+    @photos = Photo.all
+    render template: "photos/index"
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+    render template: "photos/show"
+  end
+
+  def create
+    @photo = Photo.create!(
+      image_url: params[:image_url],
+      description: params[:description],
+      date_taken: params[:date_taken],
+      title: params[:title]
+    )
+
+    render template: "photos/show"
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
